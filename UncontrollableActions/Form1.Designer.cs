c9ec0d9f5de8718fe8682ec17bd4ed4ecb4ddec9
John B Turnip <johnbturnip@gmail.com>
2016-01-13 14:57:50 -0500

    Added the console panel

--- UncontrollableActions/Form1.Designer.cs
+++ UncontrollableActions/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.consolePanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.consoleTextBox = new System.Windows.Forms.RichTextBox();
+            this.consolePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // consolePanel
+            // 
+            this.consolePanel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.consolePanel.Controls.Add(this.consoleTextBox);
+            this.consolePanel.Controls.Add(this.label1);
+            this.consolePanel.Location = new System.Drawing.Point(708, 402);
+            this.consolePanel.Name = "consolePanel";
+            this.consolePanel.Size = new System.Drawing.Size(330, 248);
+            this.consolePanel.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Console";
+            // 
+            // consoleTextBox
+            // 
+            this.consoleTextBox.Enabled = false;
+            this.consoleTextBox.Location = new System.Drawing.Point(4, 21);
+            this.consoleTextBox.Name = "consoleTextBox";
+            this.consoleTextBox.Size = new System.Drawing.Size(323, 224);
+            this.consoleTextBox.TabIndex = 1;
+            this.consoleTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 662);
+            this.Controls.Add(this.consolePanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.consolePanel.ResumeLayout(false);
+            this.consolePanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel consolePanel;
+        private System.Windows.Forms.RichTextBox consoleTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
