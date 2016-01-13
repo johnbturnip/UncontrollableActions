c5ec6273ed39a165e2f65dc1aea1a9eee7e781fe
John B Turnip <johnbturnip@gmail.com>
2016-01-13 15:45:10 -0500

    Games can now end their turn.

--- UncontrollableActions/Game.cs
+++ UncontrollableActions/Game.cs
@@ -15,9 +15,27 @@ namespace UncontrollableActions
         //Events
         public delegate void TurnEvent(int playerIndex, Actor turnPlayer);
 
+        public event TurnEvent OnTurnEnd;
+        public event TurnEvent OnTurnStart;
+
         //Private fields
         private List<Actor> playerList = new List<Actor>();
         private int currentTurnIndex = 0;
 
+
+        //Interface methods
+
+        public void NextTurn()
+        {
+            //Fire the OnTurnEnd event
+            OnTurnEnd(currentTurnIndex, CurrentTurnPlayer);
+
+            //Move to the next player's turn
+            currentTurnIndex++;
+            currentTurnIndex %= playerList.Count;
+
+            //Fire the OnTurnStart event
+            OnTurnStart(currentTurnIndex, CurrentTurnPlayer);
+        }
     }
 }
