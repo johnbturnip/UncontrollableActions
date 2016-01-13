188b71e825dabb81257c60c22077e720a8e60e54
John B Turnip <johnbturnip@gmail.com>
2016-01-13 15:36:11 -0500

    Got Game.cs started.

---
+++ UncontrollableActions/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UncontrollableActions
+{
+    public class Game
+    {
+        //Public properties
+        public Actor CurrentTurnPlayer { get { return playerList[currentTurnIndex]; } }
+        public int CurrentTurnIndex { get { return currentTurnIndex; } }
+
+        //Events
+        public delegate void TurnEvent(int playerIndex, Actor turnPlayer);
+
+        //Private fields
+        private List<Actor> playerList = new List<Actor>();
+        private int currentTurnIndex = 0;
+
+    }
+}
